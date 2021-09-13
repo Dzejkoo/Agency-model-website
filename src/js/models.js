64c14435370771c32d19b187214769557e45d18d
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-09-13 14:50:56 +0200

    Use unslpash API to download and set images of models

--- src/js/models.js
+++ src/js/models.js
@@ -2,12 +2,28 @@ export class Models {
     constructor() {
         this.apiUrl = 'https://api.unsplash.com/'
         this.apiKey = 'ujAeYpJ3CH0yUrvTA4u8xlRL2BxqXpY4jj_uEUew8h0'
+        this.photos = document.querySelectorAll('#model__image')
     }
     getModelsPhotoFromApi() {
         fetch(`${this.apiUrl}/photos/?client_id=${this.apiKey}`)
             .then(resp => resp.json())
-            .then(data => console.log(data))
+            .then(data => this.chooseSpecificDimensions(data))
 
+    }
+    chooseSpecificDimensions(array) {
+        const correctPhoto = array.filter(element => {
+            if (element.width < element.height) {
+                return element
+            }
+        });
+        this.setAtrubutePhoto(correctPhoto)
+
+    }
+    setAtrubutePhoto(correctPhoto) {
+        console.log(correctPhoto[1].urls.regular)
+        for (let i = 0; i < this.photos.length; i++) {
+            this.photos[i].setAttribute('src', correctPhoto[i].urls.regular)
+        }
     }
 
 }
