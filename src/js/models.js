e53ab35004625f14584baa81d42150d9b503170e
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-09-13 18:03:06 +0200

    Change structure of JS

--- src/js/models.js
+++ src/js/models.js
@@ -5,10 +5,9 @@ export class Models {
         this.photos = document.querySelectorAll('#model__image')
     }
     getModelsPhotoFromApi() {
-        fetch(`${this.apiUrl}/photos/?client_id=${this.apiKey}`)
+        fetch(`${this.apiUrl}/search/photos/?client_id=${this.apiKey}&query=model`)
             .then(resp => resp.json())
-            .then(data => this.chooseSpecificDimensions(data))
-
+            .then(data => this.chooseSpecificDimensions(data.results))
     }
     chooseSpecificDimensions(array) {
         const correctPhoto = array.filter(element => {
@@ -20,9 +19,9 @@ export class Models {
 
     }
     setAtrubutePhoto(correctPhoto) {
-        console.log(correctPhoto[1].urls.regular)
         for (let i = 0; i < this.photos.length; i++) {
             this.photos[i].setAttribute('src', correctPhoto[i].urls.regular)
+            this.photos[i].setAttribute('alt', correctPhoto[i].alt_description)
         }
     }
 
