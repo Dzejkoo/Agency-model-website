45c54df4bf961e8a04e160347df1588e52be9f50
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-09-14 23:11:58 +0200

    Correct revert

--- src/js/models.js
+++ src/js/models.js
@@ -5,10 +5,13 @@ export class Models {
         this.photos = document.querySelectorAll('#model__image')
     }
     getModelsPhotoFromApi() {
+<<<<<<< HEAD
         fetch(`${this.apiUrl}/photos/?client_id=${this.apiKey}`)
+=======
+        fetch(`${this.apiUrl}/search/photos/?client_id=${this.apiKey}&query=model`)
+>>>>>>> parent of 9debaf1 (What i done)
             .then(resp => resp.json())
-            .then(data => this.chooseSpecificDimensions(data))
-
+            .then(data => this.chooseSpecificDimensions(data.results))
     }
     chooseSpecificDimensions(array) {
         const correctPhoto = array.filter(element => {
@@ -20,9 +23,9 @@ export class Models {
 
     }
     setAtrubutePhoto(correctPhoto) {
-        console.log(correctPhoto[1].urls.regular)
         for (let i = 0; i < this.photos.length; i++) {
             this.photos[i].setAttribute('src', correctPhoto[i].urls.regular)
+            this.photos[i].setAttribute('alt', correctPhoto[i].alt_description)
         }
     }
 
