a7a2962ae606af076f880f874acca8730d57ba02
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-09-14 23:07:21 +0200

    Change

--- src/js/models.js
+++ src/js/models.js
@@ -5,11 +5,7 @@ export class Models {
         this.photos = document.querySelectorAll('#model__image')
     }
     getModelsPhotoFromApi() {
-<<<<<<< HEAD
         fetch(`${this.apiUrl}/search/photos/?client_id=${this.apiKey}&query=fashion`)
-=======
->>>>>>> parent of e53ab35 (Change structure of JS)
-        fetch(`${this.apiUrl}/photos/?client_id=${this.apiKey}`)
             .then(resp => resp.json())
             .then(data => this.chooseSpecificDimensions(data))
 
